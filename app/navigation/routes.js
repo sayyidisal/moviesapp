db7ca5165b955d33360604336bf92c5012d52d7f
Sayyid Shalahuddin <sayyidshalahuddin96@gmail.com>
2021-03-25 18:10:03 +0700

    first commit

---
+++ app/navigation/routes.js
@@ -0,0 +1,13 @@
+export const ROUTES = {
+  MOVIE_LIST: 'MovieList',
+  MOVIE_DETAILS: 'MovieDetails',
+  SEARCH: 'Search',
+  SEARCH_RESULTS: 'SearchResults',
+  CONFIGURATION: 'Configuration'
+};
+
+export const TABS = {
+  HOME: 'Home',
+  SEARCH: 'Search',
+  CONFIGURATION: 'More'
+};
